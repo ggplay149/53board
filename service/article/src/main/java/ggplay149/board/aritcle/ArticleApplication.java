f90e4abd9df6325e84fccc512de5e223661959cb
ggplay149 <ggplay149@gmail.com>
2025-03-24 22:31:30 +0900

    멀티모듈 환경 조성

---
+++ service/article/src/main/java/ggplay149/board/aritcle/ArticleApplication.java
@@ -0,0 +1,11 @@
+package ggplay149.board.aritcle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ArticleApplication.class,args);
+    }
+}
