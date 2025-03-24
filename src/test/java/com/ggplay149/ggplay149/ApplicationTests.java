ce4ea38f09facf8fe7c76511d4e44d6a1f229007
ggplay149 <ggplay149@gmail.com>
2025-03-24 21:43:22 +0900

    init

---
+++ src/test/java/com/ggplay149/ggplay149/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggplay149.ggplay149;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
